ee184da970ef0744b9b0512bda3dd3f0a4e76379
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2022-02-07 17:44:03 +0100

    fix: no transparency for menus

--- .config/qtile/autostart_always.sh
+++ .config/qtile/autostart_always.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-picom --fading --fade-delta=5 --inactive-opacity=1.0 --inactive-dim=0.05 &
+picom --fading --fade-delta=5 --inactive-opacity=1.0 --inactive-dim=0.05 --menu-opacity=1.0&
 conky &
