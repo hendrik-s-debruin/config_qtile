d52aba60dc9ad44f3e977bb55dca25913d1e01a7
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2022-02-08 14:32:51 +0100

    feat: transparent qutebrowser

--- .config/qtile/autostart_always.sh
+++ .config/qtile/autostart_always.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-picom --fading --fade-delta=5 --inactive-opacity=1.0 --inactive-dim=0.05 --menu-opacity=1.0&
-conky &
+picom --fading --fade-delta=5 --inactive-opacity=1.0 --inactive-dim=0.05 --menu-opacity=1.0 --opacity-rule 90:'name *= "qutebrowser"' &
+# conky &
