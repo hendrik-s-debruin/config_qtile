9b507296974858cffd596ddb4416be14e9f32921
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2022-02-10 15:43:20 +0100

    style: add newlines

--- .config/qtile/autostart_always.sh
+++ .config/qtile/autostart_always.sh
@@ -1,4 +1,9 @@
 #!/bin/env bash
 
-picom --fading --fade-delta=5 --inactive-opacity=1.0 --inactive-dim=0.05 --menu-opacity=1.0 --opacity-rule 90:'name *= "qutebrowser"' &
+picom --fading               \
+      --fade-delta=5         \
+      --inactive-opacity=1.0 \
+      --inactive-dim=0.05    \
+      --menu-opacity=1.0     \
+      --opacity-rule 90:'name *= "qutebrowser"' &
 # conky &
