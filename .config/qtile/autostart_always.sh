ce4b3b3ba99c02b6e549df6751943739b099a671
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2022-03-09 09:32:51 +0100

    style: do not make qutebrowser transparent

--- .config/qtile/autostart_always.sh
+++ .config/qtile/autostart_always.sh
@@ -4,6 +4,6 @@ picom --fading               \
       --fade-delta=5         \
       --inactive-opacity=1.0 \
       --inactive-dim=0.05    \
-      --menu-opacity=1.0     \
-      --opacity-rule 90:'name *= "qutebrowser"' &
+      --menu-opacity=1.0   & # \
+      # --opacity-rule 90:'name *= "qutebrowser"' &
 # conky &
